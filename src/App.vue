e44e5750eeac3c1d734fe487c768b87724a770ef
Youngju Chae <youngjuchae14@gmail.com>
2020-07-19 20:01:00 +1000

    simple-website

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+   <app-header v-bind:title="title" @changeTitle="updateTitle($event)"></app-header>
+   <yj-project v-bind:babies="babies"></yj-project>
+   <app-footer v-bind:title="title"></app-footer>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+import Footer from './components/Footer'
+import YJproject from './components/YJproject'
+
+export default {
+  name: 'App',
+  components: {
+    'app-header': Header,
+    'app-footer': Footer,
+    'yj-project': YJproject
+  },
+  data(){
+    return{
+       babies:[
+           {name: 'YJ', speciality: 'Boss', show: false},
+           {name: 'Hamish', speciality: 'Farting', show: false},
+           {name: 'QnQn', speciality: 'Cat punch', show: false},
+           {name: 'Heachi Girl', speciality: 'Girl Boss', show: false},
+           {name: 'Woofie', speciality: 'Barking at nothing', show: false},
+           {name: 'Chrissy', speciality: 'Cuddling', show: false}
+       ],
+       title: 'First Vue Project'
+    }
+  },
+  methods: {
+    updateTitle: function(updatedTitle){
+      this.title = updatedTitle
+    }
+  }
+  
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/YJproject.vue
@@ -0,0 +1,51 @@
+<template>
+    <div id="babies">
+        <ul>
+            <li v-for="baby in babies" :key="baby.show" @click="baby.show = !baby.show">
+                <h2>{{ baby.name }}</h2>
+                <h3 v-show="baby.show">{{ baby.speciality }}</h3>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  props: {
+      babies: {
+          type: Array
+      }
+  },
+  data(){
+    return{
+      
+    }
+  }
+  
+}
+</script>
+
+<style scoped>
+  #babies {
+    width: 100%;
+    max-width: 1200px;
+    margin: 40px auto;
+    padding: 0 20px;
+    box-sizing: border-box;
+  }
+  ul{
+    display: flex;
+    flex-wrap: wrap;
+    list-style-type: none;
+    padding: 0;
+    }
+    li{
+    flex-grow: 1;
+    flex-basis: 300px;
+    text-align: center;
+    padding: 30px;
+    border: 1px solid #222;
+    margin: 10px;
+    }
+</style>
